9a6a6196384fed048676a4693fd4f28ba0ca923d
Pranesh <Pranesh.msc@gmail.com>
2017-02-16 17:41:58 +0530

    Feature 2 ready

--- app/src/main/java/com/example/adminitrator/imdgittestapp/MainActivity.java
+++ app/src/main/java/com/example/adminitrator/imdgittestapp/MainActivity.java
@@ -9,5 +9,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This is the test code
+        /*
+
+
+
+
+
+           MultiPartEntityRequest request = new MultiPartEntityRequest(url, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                hidepDialog();
+                Log.e(TAG, "Error:" + volleyError.networkResponse);
+                NetworkResponse networkResponse = volleyError.networkResponse;
+                if (listener != null) {
+                    try {
+                        listener.onErrorResponse(tag, networkResponse.statusCode, new String(networkResponse.data));
+                    } catch (Exception e) {
+                        listener.onErrorResponse(tag, HttpStatus.SC_NO_CONTENT, new String(networkResponse.data));
+                        e.printStackTrace();
+
+
+
+
+
+         */
     }
 }
